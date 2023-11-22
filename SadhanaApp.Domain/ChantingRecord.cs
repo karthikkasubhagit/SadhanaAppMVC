fb2af5ecfc777e8c6701d87f6ce88794f6bfedee
Karthik Kasubha <karthik.kasubhaforyou@hotmail.com>
2023-11-22 15:36:11 +1300

    Reading/Hearing and Service minutes points caliculation fixed

--- SadhanaApp.Domain/ChantingRecord.cs
+++ SadhanaApp.Domain/ChantingRecord.cs
@@ -51,19 +51,18 @@ public class ChantingRecord
     {
         get
         {
-            double? scoreFromRounds = (MorningRounds.GetValueOrDefault() * 4.5) + (DayRounds.GetValueOrDefault() * 3) + (EveningRounds.GetValueOrDefault() * 1.5);
-            scoreFromRounds = Math.Min(scoreFromRounds.GetValueOrDefault(), 72);  // Cap chanting to max 72 points
+            // Chanting score calculation remains the same
+            double scoreFromRounds = (MorningRounds.GetValueOrDefault() * 4.5) +
+                                     (DayRounds.GetValueOrDefault() * 3) +
+                                     (EveningRounds.GetValueOrDefault() * 1.5);
+            scoreFromRounds = Math.Min(scoreFromRounds, 72); // Cap chanting to max 72 points
 
-            // Combine Reading and Hearing Duration
-            int? combinedDuration = ReadingDurationInMinutes + HearingDurationInMinutes;
+            // Reading & Hearing score calculation
+            double scoreFromReadingHearing = 0;
+            int combinedDuration = ReadingDurationInMinutes.GetValueOrDefault() +
+                                   HearingDurationInMinutes.GetValueOrDefault();
 
-            double? scoreFromReadingHearing;
-
-            if (!ReadingDurationInMinutes.HasValue && !HearingDurationInMinutes.HasValue)
-            {
-                scoreFromReadingHearing = 0;
-            }
-            else if (combinedDuration < 15)
+            if (combinedDuration < 15)
             {
                 scoreFromReadingHearing = -20;
             }
@@ -75,27 +74,20 @@ public class ChantingRecord
             {
                 scoreFromReadingHearing = 18;
             }
-            scoreFromReadingHearing = Math.Min(scoreFromReadingHearing.GetValueOrDefault(), 18);  // Cap reading & hearing to max 18 points
+            scoreFromReadingHearing = Math.Min(scoreFromReadingHearing, 18); // Cap reading & hearing to max 18 points
 
-            // Assuming linear points for service duration as per the description for other sections
-            double? scoreFromService;
-            if (ServiceDurationInMinutes < 15)
+            // Service score calculation
+            double scoreFromService = 0;
+            if (ServiceDurationInMinutes.GetValueOrDefault() >= 15)
             {
-                scoreFromService = 0;
+                scoreFromService = (ServiceDurationInMinutes.GetValueOrDefault() / 30.0) * 10;
             }
-            else if (ServiceDurationInMinutes <= 30)
-            {
-                scoreFromService = 10;
-            }
-            else
-            {
-                scoreFromService = 10 + (ServiceDurationInMinutes - 30) * (10.0 / 30.0);  // This could be adjusted based on exact criteria for service time
-            }
-            scoreFromService = Math.Min(scoreFromService.GetValueOrDefault(), 10);  // Cap service to max 10 points
+            scoreFromService = Math.Min(scoreFromService, 10); // Cap service to max 10 points
 
-            double? result = scoreFromRounds + scoreFromReadingHearing + scoreFromService;
-            int? roundedTotal = result.HasValue ? (int?)Math.Round(result.Value) : null;
-            return roundedTotal;
-        }
-    }
+            // Sum of all scores
+            double result = scoreFromRounds + scoreFromReadingHearing + scoreFromService;
+            return (int)Math.Round(result); // Returning the rounded total score
+        }   
+
+}
 }
